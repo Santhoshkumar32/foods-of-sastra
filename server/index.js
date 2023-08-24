5c26c641e8fe6c6a77af5f2ced194e10a495f9ce
Santhoshkumar32 <santhoshofficial32@gmail.com>
2023-08-24 14:54:20 +0530

    add root route

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json({limit: "30mb",extended: true }));
 app.use(bodyParser.urlencoded({limit: "30mb",extended: true }));
 app.use(cors());
 
+app.use('/', (req,res)=>res.send("Landing page.."));
 app.use('/posts',postRoutes);
 app.use('/user',userRoutes);
 
